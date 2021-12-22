8345181e46271026974e6dbe561358d99df775ff
Christopher Rose <christophervrose@gmail.com>
2021-12-22 09:43:03 +0530

    [fix] initdb

--- initdb/init.sql
+++ initdb/init.sql
@@ -3,6 +3,5 @@ create table models(
     model_loc varchar(128) not null ,
     data_loc varchar(128) not null ,
     pred_loc varchar(128),
-    
-
+    createdAt timestamp not null default CURRENT_TIMESTAMP
 );
