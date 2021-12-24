e8bdf41eb6b8a50f55751fc38980efe72b7fc6c0
Prajwal P <prajwalprakash3722@gmail.com>
2021-12-24 10:55:45 +0530

    [add] added readme

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ const app = express();
 const routes = require("./routes/index");
 app.use(bodyParser.json());
 app.use(cors());
-app.use(require('morgan')('dev'));
+app.use(require("morgan")("dev"));
 const port = process.env.PORT || 3001;
 app.use(routes);
 
 const startServer = async () => {
-  const mdbURL = process.env.MONGO_URI || "mongodb://localhost:27017/<database_name>"
-  await mongoose.connect(
-    mdbURL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => {
-      console.log("Connected to MongoDB",` on ${mdbURL}`);
-      app.listen(port, () => {
-        console.log(`Listening on port http://localhost:${port}`);
-      });
-    }
-  );
+    const mdbURL = process.env.MONGO_URI || "mongodb://localhost:27017/7to7";
+    await mongoose.connect(
+        mdbURL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        () => {
+            console.log("Connected to MongoDB", ` on ${mdbURL}`);
+            app.listen(port, () => {
+                console.log(`Listening on port http://localhost:${port}`);
+            });
+        }
+    );
 };
 
 startServer();
