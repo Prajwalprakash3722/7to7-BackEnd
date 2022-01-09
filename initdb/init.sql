f10aea0d72c1fc95ce3396824b95e78d9c7409db
Christopher Rose <christophervrose@gmail.com>
2022-01-09 13:49:22 +0530

    [add] sql

--- initdb/init.sql
+++ initdb/init.sql
@@ -15,6 +15,10 @@ INSERT INTO models(model_desc,model_loc,data_loc,pred_loc) VALUES
  ('Popular Bajaj Kochi-Sales','Bajaj_Sales_DT_1.1.rds','Popular Bajaj Kochi-Sales.csv','Popular Bajaj Kochi-Sales - scored.csv'),
  ('Popular Hyundai Kochi-New Car Sales','Hyundai Sales_DT_1.1.rds','Popular Hyundai Kochi-New Car Sales.csv','Popular Hyundai Kochi-New Car Sales - scored.csv'),
  ('Nandi Toyota Utrust All Branch-Non Trade-in','UTrust_NonTradeIn_DT_1.1.rds','Nandi Toyota Utrust All Branch-Non Trade-in.csv','Nandi Toyota Utrust All Branch-Non Trade-in - scored.csv');
+
+
+insert into models(model_desc, model_loc, data_loc,pred_loc, conf_loc) values('Popular Hyundai Kochi-New Car Sales With Modified Warm entries','Hyundai Sales_DT_1.1.rds','Popular Hyundai Kochi-New Car Sale With Warm.csv','Popular Hyundai Kochi-New Car Sale With Warm - scored.csv','Popular Hyundai Kochi-New Car Sale With Warm - confusion.csv');
+
 commit;
 --  ('Nandi Toyota UTrust All Branch- Trade-in','UTrust_TradeIn_DT_1.1.rds','Nandi Toyota Utrust All Branch-Trade-in.csv','Nandi Toyota Utrust All Branch-Trade-in - scored.csv');
 
